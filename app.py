26955093d1b18d7d985378b3fac9d3e050ed09d9
Jakenski <flyingjake08@gmail.com>
2018-02-12 21:18:52 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,61 @@
 from flask.ext.api import FlaskAPI, status, exceptions
 from flask import request, response
 import datetime
-#import LOFT
+import LOFT*
 
 
 
 app = FlaskAPI(__name__)
 
-#customer = [{name: ..., arrived_on: LOFT.time_come, items: ['kalyan0', 'kalyan1']}, gone: LOFT.time_gone]
+#customer = [{name: , arrived_on: LOFT.time_come, items: ['kalyan0', 'kalyan1']}, gone: LOFT.time_gone]
+customers = []
 
+def get_amount(customer_data):
+    amount = LOFT.get_total(data)
+    return amount 
 
+def validate_custumer(name,time_come,customer):
+    name = data.get('name')
+    time_come = data.get('come')
+    customer = dict.fromkeys(['name','arrived_on'],name,time_come)
+    customer_dict = json.dumps(customer)
+    return customer_dict
 
-
+def find_customer(needed_name):
+    for customer in customers.values():
+        if name = needed_name:
+            return name 
 
 @app.route('/add_customer', methods=['POST'])
 def add_customer():
-    data = request.get_json()
-  
+    data = flask.request.get_json()
     validated_customer_data = validate_customer(data)
     customers.append(validated_customer_data)
-    return response(status_code=200, data={'foo': 1, 'bar': 2))
+    return jsonify(status_code=200, validated_customer_data)
 
 
 @app.route('/checkout_customer', methods=['POST'])
 def checkout_customer():
-    data = request.get_json()
+    """The guest counts"""
+    data = flask.request.get_json()
     customer_data = find_customer(data)
     amount = get_amount(customer_data)
     remove_customer(customer)
-
     return response({'amount': amount})
+
+
 
 
 @app.route('/add_item', methods=['POST'])
 def add_item():
-    data = request.get_json()
-    user = find_customer(data)
+    """Add an additional service"""
+    data = flask.request.get_json()
+    customer = find_customer(data)
     add_item(item, customer)
     return response({'amount': amount, 'item':item})
 
 
-@app.route('/get_customer', methods=['GET'])
+@app.route('/get_customer', methods=['POST'])
 def get_customer():
     return costomers
 
@@ -49,4 +64,5 @@ def get_customer():
     
 
 app.run()
+
 
